4f1ec44c69e2fdec2c2e433a9fbf9af16a341fed
Bogini.Anitha <129845617+90144@users.noreply.github.com>
2026-03-24 19:41:26 +0530

    Delete vowels.py

--- vowels.py
+++
@@ -1,9 +0,0 @@
-words=["Apple","Banana","Cherry","Date","Elderberry"]
-vowels="AEIOUaeiou"
-for word in words: 
-    count=0
-    for char in word: 
-        if char in vowels: 
-            print(char, end=" ") 
-            count += 1
-    print(f"Number of vowels in {word}: {count}")
